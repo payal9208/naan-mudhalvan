4ea081e829de0ae03cb23635ada967ec79f46248
Payal S <payalparasuram@gmail.com>
2026-08-12 11:19:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def recommend():
         return f"Error generating recommendations: {e}"
 
   if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", 
+port=5000, debug=False)
